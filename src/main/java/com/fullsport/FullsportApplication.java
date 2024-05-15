9d350b97f9dc4c4f2a6e6e7b18b4fa517df23bca
carlos_manjarres <manjarresc63@gmail.com>
2024-05-15 08:39:49 -0500

    primer commit del spring

---
+++ src/main/java/com/fullsport/FullsportApplication.java
@@ -0,0 +1,13 @@
+package com.fullsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullsportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullsport/FullsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
